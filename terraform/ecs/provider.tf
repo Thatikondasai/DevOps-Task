39c98f0d9a28b9d9620caf7f49ef7c4e8f22c972
Thatikondasai <56509559+Thatikondasai@users.noreply.github.com>
2022-09-25 15:42:10 +0530

    Update provider.tf

--- terraform/ecs/provider.tf
+++ terraform/ecs/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIA4AFSVYK7OONBSS5H"
-  secret_key = "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"
+  access_key = ""
+  secret_key = "C"
   region     = var.aws_region
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   #shared_credentials_file = "$HOME/.aws/credentials"
